3be9fb477e87fde55fdf6d753c932804c852e4be
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-23 19:05:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  
+  function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Export functions
+  module.exports = { writeCards, countDown };
+  
